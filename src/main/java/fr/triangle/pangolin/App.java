f362a28ab5620da65ea956f9ab22047fbe3e3634
Marius <marius.monnier@etu.univ-grenoble-alpes.fr>
2020-04-08 11:30:10 +0200

    coucou

---
+++ src/main/java/fr/triangle/pangolin/App.java
@@ -0,0 +1,13 @@
+package fr.triangle.pangolin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
