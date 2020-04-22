bd17e4977f1cb0eaa6010d7313993f08881e18e1
Marius <marius.monnier@etu.univ-grenoble-alpes.fr>
2020-04-22 11:51:11 +0200

    Ajout du try and catch

--- src/main/java/fr/triangle/pangolin/App.java
+++ src/main/java/fr/triangle/pangolin/App.java
@@ -11,10 +11,14 @@ import fr.triangle.pangolin.dataframe.Dataview;
  */
 public class App 
 {
-    public static void main( String[] args ) throws FileNotFoundException
+    public static void main( String[] args )
     {
+    	try {
         Dataframe d = new Dataframe("./annexes/testGoodCSV.csv");
         Dataview dv = new Dataview(d);
         dv.printAll(System.out);
+    	} catch(FileNotFoundException e) {
+    		
+    	}
     }
 }
