ad59594c9e61d4f5b339f51c0af917fe4d813684
clement <chlous.clem@gmail.com>
2020-04-24 10:44:20 +0200

    dataview Magnifique

--- src/main/java/fr/triangle/pangolin/App.java
+++ src/main/java/fr/triangle/pangolin/App.java
@@ -14,9 +14,26 @@ public class App
     public static void main( String[] args )
     {
     	try {
-        Dataframe d = new Dataframe("./annexes/testGoodCSV.csv");
+        Dataframe d = new Dataframe("./annexes/testGoodCSVLong.csv");
         Dataview dv = new Dataview(d);
         dv.printAll(System.out);
+        System.out.println();
+        dv.printBeatifull(System.out);
+        System.out.println();
+        dv.printFirst(System.out);
+        System.out.println();
+        dv.printFirst(System.out, 12);
+        System.out.println();
+        dv.printLast(System.out);
+        System.out.println();
+        dv.printLast(System.out, 13);
+        System.out.println();
+        dv.printFirst(System.out, 30);
+        System.out.println();
+        dv.printLast(System.out);
+        System.out.println();
+        dv.printLast(System.out, 30);
+        System.out.println();
     	} catch(FileNotFoundException e) {
     		
     	}
--- src/main/java/fr/triangle/pangolin/dataframe/Dataview.java
+++ src/main/java/fr/triangle/pangolin/dataframe/Dataview.java
@@ -5,46 +5,72 @@ import java.io.PrintStream;
 public class Dataview implements View{
 
 	Dataframe d;
+	int nbLigne; 
+	int nbCol;
 	public Dataview(Dataframe dataframe) {
 		d = dataframe;
+		nbLigne = d.lines.size(); 
+		nbCol = d.columns.size();
 	}
 
-	@Override
-	public void printAll(PrintStream ps) {
-		for(int j = 0; j<d.columns.size(); j++) {
+	
+	public void printBeatifull(PrintStream ps) {
+		for(int j = 0; j<nbCol; j++) {
+			
 			ps.print(d.columns.get(j).label + "\t|\t");
 		}
 		ps.println("\n-------------------------");
-		for(int i = 0; i<d.lines.size(); i++) {
+		for(int i = 0; i<nbLigne; i++) {
 			for(int k = 0; k<d.lines.get(0).length; k++) {
 				ps.print(d.lines.get(i).get(k) + "\t|\t");
 			}
 			ps.println();
 		}
+	}
+	
+	@Override
+	public void printAll(PrintStream ps) {
+		printPrecise(ps, 1, nbLigne);
+	}
+	
+	//pour les 3 premieres s = 1 et e = 3
+	private void printPrecise(PrintStream ps, int s, int e) {
+		
+		int max = Math.min(e, nbLigne);
+		
+		for(int j = 0; j<nbCol-1; j++) {
+			ps.print(d.columns.get(j).label + ";");
+		}
+		ps.println(d.columns.get(nbCol-1).label); //le dernier label
+		for(int i = s-1; i<max; i++) {
+			for(int k = 0; k<d.lines.get(0).length-1; k++) {
+				ps.print(d.lines.get(i).get(k) + ";");
+			}
+			ps.println(d.lines.get(i).get(d.lines.get(0).length-1)); //le denier argument de la ligne
+		}
+		ps.println();
 		
 	}
-
 	@Override
 	public void printFirst(PrintStream ps) {
-		// TODO Auto-generated method stub
+		printPrecise(ps, 1, 10);
 		
 	}
 
 	@Override
 	public void printFirst(PrintStream ps, int n) {
-		// TODO Auto-generated method stub
+		printPrecise(ps, 1, n);
 		
 	}
 
 	@Override
 	public void printLast(PrintStream ps) {
-		// TODO Auto-generated method stub
-		
+		printPrecise(ps, Math.max(1, nbLigne-9), nbLigne);
 	}
 
 	@Override
 	public void printLast(PrintStream ps, int n) {
-		// TODO Auto-generated method stub
+		printPrecise(ps, Math.max(1, nbLigne-n+1), nbLigne);
 		
 	}
 
